c9893694306df849f96bb136aa97145c49a71e42
nyancodeid <ryandevstudio@gmail.com>
2017-06-26 04:46:09 +0700

    update test

--- test.js
+++ test.js
@@ -25,16 +25,4 @@ describe('imageCache Test', function() {
 			});
 		});
 	});
-
-	describe('#getCache()', function() {
-		it('should be return error 301 moved', function(done) {
-			var url = ['https://lh3.googleusercontent.com/-AUpXPK4IOi4/AAAAAAAAAAI/AAAAAAAAAAA/AI6yGXxcuACwUIVtH8VfdOlCD8KQjDDZSw/s32-c-mo/photo.jpg'];
-
-			imageCache.setCache(url, {googleCache: true, resize: false},function(error) {
-				console.log(url);
-				
-				done();
-			});
-		});
-	});
 });
